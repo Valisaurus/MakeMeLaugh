a54050b72c2e5b87817b55dda032075a42efc8ec
Valisaurus <valley.alosachi@hotmail.com>
2022-12-06 15:37:08 +0100

    mobile/desktop, click vs mouseover

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//array with laughter sounds
 const laughs = [
   {
     name: 'intenseLaugh',
@@ -36,8 +37,6 @@ const laughter = document.querySelector('source');
 
 const main = document.querySelector('main');
 
-// console.log(laughter);
-
 //creating a button
 const createButton = () => {
   const button = document.createElement('button');
@@ -45,10 +44,13 @@ const createButton = () => {
   main.append(button);
   return button;
 };
+//puts the function createButton in the variable button
 const button = createButton();
 
 //starts on index 0 in the array
 let currentLaughIndex = 0;
+
+//when clicking on the button a laughter plays and when clicking again another laughter plays. The Laughters are stored in a laughs array.
 
 button.addEventListener('click', () => {
   const audio = document.querySelector('audio');
@@ -59,24 +61,41 @@ button.addEventListener('click', () => {
   if (currentLaughIndex > laughs.length - 1) {
     currentLaughIndex = 0;
   }
-  // console.log(laughter);
 });
-
-// const heading = document.querySelector('h1');
 
 const body = document.querySelector('body');
 
-button.addEventListener('mouseover', () => {
-  for (i = 0; i < 2; i++) {
-    const mouth = document.createElement('img');
-    mouth.src = './images/mouth.png';
-    mouth.classList.add('mouth');
-    mouth.style.top = Math.random() * window.innerHeight + 'px';
-    mouth.style.left = Math.random() * window.innerWidth + 'px';
-    mouth.style.width = Math.random() * (200 + 100) + 'px';
-    body.appendChild(mouth);
+//mobile version
+const mobileVersion = () => {
+  if (window.innerWidth < 500) {
+    //when hovering over the button mouth images are randomly displayed
+    button.addEventListener('click', () => {
+      for (i = 0; i < 2; i++) {
+        const mouth = document.createElement('img');
+        mouth.src = './images/mouth.png';
+        mouth.classList.add('mouth');
+        mouth.style.top = Math.random() * window.innerHeight + 'px';
+        mouth.style.left = Math.random() * window.innerWidth + 'px';
+        mouth.style.width = Math.random() * (100 + 100) + 'px';
+        body.appendChild(mouth);
+      }
+    });
+  } else {
+    //when hovering over the button mouth images are randomly displayed
+    button.addEventListener('mouseover', () => {
+      for (i = 0; i < 2; i++) {
+        const mouth = document.createElement('img');
+        mouth.src = './images/mouth.png';
+        mouth.classList.add('mouth');
+        mouth.style.top = Math.random() * window.innerHeight + 'px';
+        mouth.style.left = Math.random() * window.innerWidth + 'px';
+        mouth.style.width = Math.random() * (100 + 100) + 'px';
+        body.appendChild(mouth);
+      }
+    });
   }
-});
+};
+mobileVersion();
 
 // button.addEventListener('mouseleave', () => {
 //   const mouths = document.querySelectorAll('mouth');
